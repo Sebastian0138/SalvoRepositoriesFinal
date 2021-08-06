4824a02d6fc9b6ae1281e7abc3c30cad6f7d48fb
MaurinJuan <maurinjuanpablo@gmail.com>
2021-08-06 16:41:12 -0300

    Revert "Cambios raraos de prueba jsjs"
    This reverts commit 0e182e9ce9f76987ce2a71ec1b0478e458efb2d6.

--- src/main/java/com/codeoftheweb/salvo/Player.java
+++ src/main/java/com/codeoftheweb/salvo/Player.java
@@ -12,7 +12,7 @@ public class Player {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
-    @GenericGenerator(name = "nat", strategy = "native")
+    @GenericGenerator(name = "native", strategy = "native")
     private Long id;
         private String userName;
 
